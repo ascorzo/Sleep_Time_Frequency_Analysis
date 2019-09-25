d6674e51940f37c7769729e77c9e06f5e983786a
Andrea <adsanchez1@uc.cl>
2019-09-24 22:40:30 -0700

    Last changes

--- Plot_statistics_ConnectPSD.m
+++ Plot_statistics_ConnectPSD.m
@@ -1,4 +1,20 @@
 
+filepath = 'C:\Users\lanan\Documents\MATLAB\functions\Scripts Marcos';
+addpath(genpath(filepath))
+%% Input params
+
+display_fig = 'on';   % 'on' or 'off'
+savefig = 0;           % binario 1 = guardar, 0 = no guardar
+% t = 2;                 % COMPARE FREQS (1) OR TASKS (2)
+testType = 'signrank'; % type of wilcoxon or statistical test
+windowSize = 10;       % non-overlapping window size for test % 32 = 250 ms, 63 = 500 ms,  96 = 750
+start_point = 1;     % start sample for testing (usually after baseline), 1537 (500 ms), 126 = 0 189 (500 ms)
+
+error = 'Sem';         % define error
+
+x1 = 0; % events line plots in seconds
+freq = 'PSD'; 
+
 %% Plot PSD of all scouts
 
 for scout = 1:numel(scouts)-1
@@ -26,8 +42,8 @@ for scout = 1:numel(scouts)-1
 end
 %% Plot connectivity PSD of all scouts
 
-for s_scout = 1:5%numel(scouts)
-    for s_scout2 = s_scout:numel(scouts)%s_scout:numel(scouts)
+for s_scout = 1:5%numel(scouts)|
+    for s_scout2 = s_scout+1:numel(scouts)%s_scout:numel(scouts)
         Data_connect_OdorOn = ...
             squeeze(squeeze(permute(CodorOn(:,s_scout,s_scout2,:),[2,3,1,4])));
         
@@ -59,3 +75,125 @@ for s_scout = 1:5%numel(scouts)
         legend('odor', 'placebo');
     end
 end
+
+%% Difference between on and off in PSD of all scouts
+
+for scout = 1:numel(scouts)-1
+    
+    Data_PSD_OdorOn = log10(squeeze(permute(PSD_OdorOn(2:end,scout,:),[2,1,3])));
+    Data_PSD_OdorOff = log10(squeeze(permute(PSD_OdorOff(2:end,scout,:),[2,1,3])));
+    
+    Data_PSD_OdorDiff = Data_PSD_OdorOn - Data_PSD_OdorOff;
+    
+    Data_PSD_PlacOn = log10(squeeze(permute(PSD_PlacOn(2:end,scout,:),[2,1,3])));
+    Data_PSD_PlacOff = log10(squeeze(permute(PSD_PlacOff(2:end,scout,:),[2,1,3])));
+
+    Data_PSD_PlacDiff = Data_PSD_PlacOn - Data_PSD_PlacOff;
+
+    
+    ROI = scouts(scout);
+    figure
+    %subplot(2,1,1);
+    shadedErrorBar(v_FreqAxis,Data_PSD_OdorDiff,{@mean,@std},'lineprops', '-b');hold on;
+    shadedErrorBar(v_FreqAxis,Data_PSD_PlacDiff,{@mean,@std},'lineprops', '-k');hold on;
+    title(strcat('pow spectrum area 1 Odor',ROI))
+    legend('Odor', 'Placebo');
+    
+end
+
+%% Difference between on and off in connectivity PSD of all scouts
+
+for s_scout = 1:5%numel(scouts)|
+    for s_scout2 = s_scout + 1:numel(scouts)%s_scout:numel(scouts)
+        Data_connect_OdorOn = ...
+            squeeze(squeeze(permute(CodorOn(:,s_scout,s_scout2,:),[2,3,1,4])));
+        
+        Data_connect_OdorOff = ...
+            squeeze(squeeze(permute(CodorOff(:,s_scout,s_scout2,:),[2,3,1,4])));
+        
+        Data_connect_OdorDiff = Data_connect_OdorOn - Data_connect_OdorOff;
+        
+        Data_connect_PlacOn = ...
+            squeeze(squeeze(permute(CPlacOn(:,s_scout,s_scout2,:),[2,3,1,4])));
+        
+        Data_connect_PlacOff = ...
+            squeeze(squeeze(permute(CPlacOff(:,s_scout,s_scout2,:),[2,3,1,4])));
+        
+        Data_connect_PlacDiff = Data_connect_PlacOn - Data_connect_PlacOff;
+        
+        ROI1 = scouts(s_scout);
+        ROI2 = scouts(s_scout2);
+        
+        roi = strcat(ROI1,{' vs '},ROI2);
+        
+        condition1 = 'Odor'; condition2 = 'Placebo';
+         
+        [stats] = test_wilcoxon_cvar(Data_connect_OdorDiff,Data_connect_PlacDiff,...
+            condition1, condition2, testType, windowSize, start_point);
+        
+        yaxis = [min(min([Data_connect_OdorDiff;Data_connect_PlacDiff]))-5,...
+            max(max([Data_connect_OdorDiff;Data_connect_PlacDiff]))+5]; 
+        
+        %-------Plot configuration-------
+        
+        Xt = v_FreqAxis;%EjeX; % Time o EjeX
+        
+        figure1 = roi;
+        figure('visible',display_fig,'position', [0, 0, 1400, 700]); hold on;
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        shadedErrorBar(Xt,Data_connect_OdorDiff,{@mean,@std},'lineprops', '-b');hold on;
+        shadedErrorBar(Xt,Data_connect_PlacDiff,{@mean,@std},'lineprops', '-k');hold on;
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        xlabel('Time (ms)','FontSize',12,'FontWeight','bold')
+        ylabel(['11 - 15.5Hz ' freq],'FontSize',12,'FontWeight','bold')
+        %%%%%%%%%%%%%%%%%%%%%%%%%% AXIS SCALE %%%%
+        %ylim([yaxis(1) yaxis(2)])
+        legend('Odor','Placebo')
+        
+        title(figure1,'Fontsize',12,'FontWeight','bold','interpreter', 'none');
+        h = title(figure1,'Fontsize',12,'FontWeight','bold','interpreter', 'none');
+        P = get(h,'Position');
+        set(h,'Position',[P(1) P(2)+0.1 P(3)])
+        
+        ax = gca; % current axes
+        ax.XTickMode = 'manual';
+        ax.TickDirMode = 'manual';
+        ax.TickDir = 'in';
+        ax.XColor = 'black';
+        ax.YColor = 'black';
+        set(gca,'LineWidth',2,'Fontsize',12,'FontWeight','bold','clipping', 'on')
+        
+        %--- Sombreado--------------
+        pmask = stats.p_masked;
+        winlim = stats.movwind;
+        
+        for pv=1:length(pmask)
+            
+            if pmask(pv) == 1
+                p = patch('Faces', [1 2 3 4], 'Vertices', [Xt(winlim(pv)) yaxis(1); Xt(winlim(pv)) yaxis(2);...
+                    Xt(winlim(pv+1)) yaxis(2); Xt(winlim(pv+1)) yaxis(1)]);
+                set(p,'facealpha',.2,'linestyle','none');
+            end
+            
+        end
+        
+        plot([x1(1) x1(1)],[yaxis(1)+0.05 yaxis(2)-0.05],'color','k','LineWidth',2,'LineStyle','-');
+        hold on;
+        set(gca,'units','pix','pos',[100 100 1130 400])
+        box on;
+        
+%         figure
+%         shadedErrorBar(v_FreqAxis,Data_connect_OdorDiff,{@mean,@std},'lineprops', '-b');hold on;
+%         shadedErrorBar(v_FreqAxis,Data_connect_PlacDiff,{@mean,@std},'lineprops', '-k');
+%         title(strcat('pow spectrum area 1 On',ROI1,ROI2))
+%         legend('odor', 'placebo');
+%         
+        
+    end
+end
+
+
+
+
+
+
--- Plot_statistics_TimeCoher.m
+++ Plot_statistics_TimeCoher.m
@@ -25,10 +25,11 @@ x1 = 0; % events line plots in seconds
 
 %% para comparar tareas %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-v_nscouts = [1 2 3 5 6 9 11 14 15 17 20 21 22 23 24 29 33 34 40 41 42];
+%v_nscouts = [1 2 3 5 6 9 11 14 15 17 20 21 22 23 24 29 33 34 40 41 42];
+v_nscouts = [22 23 33 29 41 42];
 %v_nscouts = 23:1:30;
-for s_scout = 1:numel(v_nscouts)
-    for s_scout2 = s_scout:numel(v_nscouts)
+for s_scout = 4 %1:numel(v_nscouts)
+    for s_scout2 = 1:numel(v_nscouts)%s_scout:numel(v_nscouts)
         if (s_scout2 ~= s_scout) %&& (scout<= ceil(numel(scouts)/2))
             % para correr esto es necesario definir que t es distinto de 1 y los rois
             freq = '(coherence)';   % en minusculas           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -49,10 +50,15 @@ for s_scout = 1:numel(v_nscouts)
             % grupo = [condition1 ' and ' condition2 ' - Mean ' freq ' Z-score - SEM - ROI ' roi ];
             grupo = roi;
             
-            c1 = squeeze(permute(SpindlePowerCohOdor(:,scout,scout2,:),[2,3,1,4])); % Vector de Condicion 1
-            c2 = squeeze(permute(SpindlePowerCohPlac(:,scout,scout2,:),[2,3,1,4])); %Vector de Condicion 2
+            if scout<scout2
+                c1 = squeeze(permute(SpindlePowerCohOdor(:,scout,scout2,:),[2,3,1,4])); % Vector de Condicion 1
+                c2 = squeeze(permute(SpindlePowerCohPlac(:,scout,scout2,:),[2,3,1,4])); %Vector de Condicion 2
+            else
+                c1 = squeeze(permute(SpindlePowerCohOdor(:,scout2,scout,:),[2,3,1,4])); % Vector de Condicion 1
+                c2 = squeeze(permute(SpindlePowerCohPlac(:,scout2,scout,:),[2,3,1,4])); %Vector de Condicion 2
+            end
             
-            yaxis = [min(min([c1;c2]))-5 max(max([c1;c2]))+5];        % define yaxis limits
+            yaxis = [10 60];%[min(min([c1;c2]))-5 max(max([c1;c2]))+5];        % define yaxis limits
 
             %% run stats (Wilcoxon ranksum or signed test) and save
             
@@ -66,24 +72,25 @@ for s_scout = 1:numel(v_nscouts)
             Xt = v_TimeAxisCoh;%EjeX; % Time o EjeX
             
             figure1 = grupo;
-            figure('visible',display_fig,'position', [0, 0, 1400, 700]); hold on;
+            figure%('visible',display_fig,'position', [0, 0, 1000, 500]); hold on;
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %shadedErrorBar(Xt, mean(c1,1), SEM_calc(c1,error),'b',1); hold on;           % control blue
             %shadedErrorBar(Xt, mean(c2,1), SEM_calc(c2,error),'g',1); hold on;            % cm green
-            shadedErrorBar(Xt,c1,{@mean,@std},'lineprops', '-b');hold on;
-            shadedErrorBar(Xt,c2,{@mean,@std},'lineprops', '-k');hold on;
+            p1 = shadedErrorBar(Xt,c1,{@mean,@std},'lineprops', '-b');hold on;
+            p2 = shadedErrorBar(Xt,c2,{@mean,@std},'lineprops', '-k');hold on;
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            xlabel('Time (ms)','FontSize',12,'FontWeight','bold')
+            xlabel('Time (s)','FontSize',12,'FontWeight','bold')
             ylabel(['11 - 15.5Hz ' freq],'FontSize',12,'FontWeight','bold')
             %%%%%%%%%%%%%%%%%%%%%%%%%% AXIS SCALE %%%%
             ylim([yaxis(1) yaxis(2)])
             xlim ([s_baselineStartTime max(Xt)])
-            legend('Odor','Placebo')
+            %legend('Odor','Placebo')
+            
             
             title(figure1,'Fontsize',12,'FontWeight','bold','interpreter', 'none');
             h = title(figure1,'Fontsize',12,'FontWeight','bold','interpreter', 'none');
             P = get(h,'Position');
-            set(h,'Position',[P(1) P(2)+0.1 P(3)])
+            %set(h,'Position',[P(1) P(2)+0.1 P(3)])
             
             ax = gca; % current axes
             ax.XTickMode = 'manual';
@@ -96,20 +103,7 @@ for s_scout = 1:numel(v_nscouts)
             ax.YColor = 'black';
             set(gca,'LineWidth',2,'Fontsize',12,'FontWeight','bold','clipping', 'on')
             
-            % legend lines, stats lines, mask, events lines
-            %L(1) = plot(nan, nan, 'r-','linewidth',1);
-            %L(2) = plot(nan, nan, 'k-','linewidth',1);
-            %legend(L, {condition1, condition2}); hold on;
-            %legend('boxoff')
-            
-            % g = alphamask(~mask,[0 0 0],0.2); % oscuro es cercano a 1
-            % set(g,'YData',[10 0]);
-            % set(g);
-            
-            % plot(Xt(start_point:end),...
-            %     double(stats.mask(start_point:end))-2,...
-            %     'color','r','LineWidth',3,'LineStyle','-');
-            
+                       
             %% Sombreado
             pmask = stats.p_masked;
             winlim = stats.movwind;
@@ -124,36 +118,13 @@ for s_scout = 1:numel(v_nscouts)
                 
             end
             
-            plot([x1(1) x1(1)],[yaxis(1)+0.05 yaxis(2)-0.05],'color','k','LineWidth',2,'LineStyle','-');
-            %plot([x1(1) x1(1)],'color','k','LineWidth',2,'LineStyle','-');
-            % plot([x1(2) x1(2)],[yaxis(1)+0.05 yaxis(2)-0.05],'color','k','LineWidth',2,'LineStyle',':');
-            % plot([x1(3) x1(3)],[yaxis(1)+0.05 yaxis(2)-0.05],'color','k','LineWidth',2,'LineStyle',':');
+            p3 = plot([x1(1) x1(1)],[yaxis(1)+0.05 yaxis(2)-0.05],'color','k','LineWidth',2,'LineStyle','-');
             hold on;
-            set(gca,'units','pix','pos',[100 100 1130 400])
+            %set(gca,'units','pix','pos',[100 100 1130 400])
             box on;
-            
+       
         end
+        %legend([p1,p2],{'Odor','Placebo'})
     end
 end
-
-
-% save to png and tiff 200 dpi transparente sin antializing
-% if savefig == 1;
-%     export_fig(figure1,'-png','-transparent','-a1','-r200',gca)
-%     disp('FIGURE SAVED')
-% else
-%     disp('FIGURE NOT SAVED - To save figure, change savefig to 1.')
-% end
-
-% disp('                           ')
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Save values as a table
-
-% summary = table();
-% summary.winstart = Xt(winlim(1:end-1))';
-% summary.windend = Xt(winlim(2:end))';
-% summary.pvalue = stats.pval';
-% summary.pthresh = pmask;
-% summary.zval = [stats.statval.zval]';
 
